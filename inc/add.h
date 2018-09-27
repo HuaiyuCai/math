9819d11b3ae42603032e1bcf6109c99c9eb7652c
caihuaiyu <caihuaiyu123@gmail.com>
2018-09-27 23:42:27 +0800

    add add.h add.c

---
+++ inc/add.h
@@ -0,0 +1,9 @@
+
+#if !defined(_ADD_H_)
+#define _ADD_H_
+
+#include "../common_header/datatype.h"
+
+int add(int a, int b);
+
+#endif // _ADD_H_
---
+++ src/add.c
@@ -0,0 +1,6 @@
+#include "../inc/add.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
