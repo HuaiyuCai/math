ada04d93eaf365e3144bb1aea1db366c45eb7c99
caihuaiyu <caihuaiyu123@gmail.com>
2018-09-28 23:49:30 +0800

    add sub.h sub.c

---
+++ inc/sub.h
@@ -0,0 +1,9 @@
+
+#if !defined(_SUB_H_)
+#define _SUB_H_
+
+#include "../../common_header/datatype.h"
+
+int32 sub(int32 a, int32 b);
+
+#endif // _SUB_H_
---
+++ src/sub.c
@@ -0,0 +1,7 @@
+#include "../inc/sub.h"
+#include "../../common_header/datatype.h"
+
+int32 sub(int32 a, int32 b)
+{
+    return a - b;
+}
